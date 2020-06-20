c7b8705e1cd79f9818b281a4a25c5d1975536f34
Avisrara74 <avisrara74@gmail.com>
2020-06-20 15:44:12 +0500

    Updates

---
+++ precache-manifest.5459ee02dacc44b252489b97e955bc13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89efecb1bced5ccdd101cba9b6cd090",
+    "url": "/JM_formik-node-express/index.html"
+  },
+  {
+    "revision": "b6e2fd8c104cdcdffb5f",
+    "url": "/JM_formik-node-express/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "cb6a61867b9da65dbd8a",
+    "url": "/JM_formik-node-express/static/css/main.d4ad545c.chunk.css"
+  },
+  {
+    "revision": "b6e2fd8c104cdcdffb5f",
+    "url": "/JM_formik-node-express/static/js/2.77b0faeb.chunk.js"
+  },
+  {
+    "revision": "4f160e29840e1f64ec2468f8c9029bf3",
+    "url": "/JM_formik-node-express/static/js/2.77b0faeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6a61867b9da65dbd8a",
+    "url": "/JM_formik-node-express/static/js/main.8fff8d74.chunk.js"
+  },
+  {
+    "revision": "a1982ec8336a6aadb690",
+    "url": "/JM_formik-node-express/static/js/runtime-main.c82bfd56.js"
+  }
+]);
